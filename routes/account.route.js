d3cefc469c30996093d26e7a5b61462c1b1a1dd4
rajkumar-cmd <raymarhossor@gmail.com>
2023-04-08 12:46:49 +0530

    masai-bank

--- routes/account.route.js
+++ routes/account.route.js
@@ -3,27 +3,23 @@ const { userModel } = require("../models/account.model");
 const jwt = require("jsonwebtoken")
 const userRouter = express.Router();
 
-userRouter.get("/",(req,res)=>{
+userRouter.get("/", (req, res) => {
     res.send("Home Page")
 })
 userRouter.post("/openAccount", async (req, res) => {
     const { Name, Gender, DOB, Email, Mobile, InitialBalance, AdharNo, PanNo } = req.body;
-    // try {
-        const users = await userModel.find({ Name });
-        if (users.length > 0) {
-            const token=jwt.sign({userName:users[0].name},"masaiBank")
-            res.send({ "msg": "Already Registered","token":token })
-        } else {
-            const user = new userModel({ Name, Gender, DOB, Email, Mobile, InitialBalance, AdharNo, PanNo });
-            await user.save();
-            const token=jwt.sign({userName:users.name},"masaiBank")
-            res.send({ "msg": "Registered Successfully","token":token })
-        }
-    // } catch (err) {
-        // res.send({ "msg": "Registeration Failed" })
-    // }
+    const users = await userModel.find({ Name });
+    if (users.length > 0) {
+        const token = jwt.sign({ userName: users[0].name }, "masaiBank")
+        res.send({ "msg": "Already Registered", "token": token,"details":users })
+    } else {
+        const user = new userModel({ Name, Gender, DOB, Email, Mobile, InitialBalance, AdharNo, PanNo });
+        await user.save();
+        const token = jwt.sign({ userName: user.name }, "masaiBank")
+        res.send({ "msg": "Registered Successfully", "token": token , "details":user})
+    }
 })
 
-module.exports={
+module.exports = {
     userRouter
 }
